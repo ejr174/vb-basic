377e553a851350ff8ddf97af013134a99e18b63c
Emmanuel Jaramillo <ejaramillor@tcc.com.co>
2025-09-11 22:12:14 -0500

    chore: limpiar repo, ignorar .vs y artefactos de compilación

--- vb-basic/vb-basic/Module1.vb
+++ vb-basic/vb-basic/Module1.vb
@@ -1,6 +1,8 @@
-﻿Module Module1
-
+﻿'Los modulos permiten separar las diferentes secciones en .vb
+Module Module1
+    ' Dentro de los módulo tenemos "sub rutinas"
     Sub Main()
+        Console.WriteLine("Hola mundo")
 
     End Sub
 
