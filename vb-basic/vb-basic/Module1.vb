cba4a15338c3df9013056f14e9dc504d53612a74
Emmanuel Jaramillo <ejaramillor@tcc.com.co>
2025-09-11 22:44:32 -0500

    En esta clase modificamos los colores de la consola con Foregroundcolor() y Backgroundcolor()

--- vb-basic/vb-basic/Module1.vb
+++ vb-basic/vb-basic/Module1.vb
@@ -2,9 +2,17 @@
 Module Module1
     ' Dentro de los módulo tenemos "sub rutinas"
     Sub Main()
-        Console.WriteLine("Hola mundo")
-        Console.WriteLine("Presiona una letra")
-        Console.ReadKey()
+        'Diseño de la consola (opcional-practica)
+        Console.ForegroundColor = ConsoleColor.White 'Foreground es el frente
+        Console.BackgroundColor = ConsoleColor.Cyan ' Background es el fondo
+        Console.Clear() ' Ayuda a corregir un problema con las consolas cuando se les modifica las propiedades
+
+        'Mensajes que se muestran en consola 
+        Console.WriteLine("Hola, la consola se ejecuto correctamente.")
+        Console.WriteLine("Presiona una letra para salir ->")
+
+        'Metodo para mantener la consola abierta hasta que presione una letra
+        Console.ReadKey() 'También podríamos utiliza Console.ReadLine
     End Sub
 
 End Module
