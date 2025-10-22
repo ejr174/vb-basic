9a17db7560e879d125d238f66682ebc7bffaa8b0
Emmanuel Jaramillo <ejaramillor@tcc.com.co>
2025-10-21 21:21:48 -0500

    Fundamentos POO

--- vb-basic/vb-basic/Module1.vb
+++ vb-basic/vb-basic/Module1.vb
@@ -207,6 +207,8 @@ Module Module1
         Console.WriteLine("------------ Foreach -----------")
         Foreach()
 
+        Console.WriteLine("------------ POO: CLASE AUTO -----------")
+        instanciarAuto()
 
         '--------------------------------------------------------------------------------------
         'Metodo para mantener la consola abierta hasta que presione una letra
@@ -414,6 +416,88 @@ Module Module1
             'Si necesita el indice descomentar pero no es recomendado, si necesita indice utilizar For normal
             'j += 1
         Next
+    End Sub
+
+    ' POO
+    ' POO: Es un paradigma de programación; Es una manera de programar especifica, donde se organiza el código en unidades denominadas clases.
+    ' Las clases crean objetos que se relacioan entre sí para conseguir los objetivos de las aplicaciones.
+
+    ' La POO es una forma especial de programar, más cercana a cómo expresaríamos las cosas en la vida real que otros tipo de progración.
+
+    ' Ejemplo: Tenemos un OBJETO carro, este carro  tiene CARACTERISTICAS como: Color - Marca - Modelo.
+    ' El carro tambien tiene unas FUNCIOANLIDADES como: Encender - Apagar- Acelerar - Frenar
+
+    ' Entonces en POO, una CLASE es una abstracción de un OBJETO. Retomando el ejemplo del auto...
+    ' la CLASE Auto tiene unos ATRIBUTOS (caracteristicas) y tambien MÉTODOS (funcionalidades)
+
+    ' Instancia: Cuando se crea un objeto apartir de una clase. Entonces el nuevo objeto creado es una instancia de la clase.
+    ' Las instancias ya tienen atributos y metodos definidos por la clase, pero estos se pueden modificar si es necesario.
+
+    ' MÉTODOS Y ATRIBUTOS PUBLICOS Y PRIVADOS
+    ' En POO podemos definitr si los atributo o metodos son "PUBLIC" o "PRIVATE", con esto podemos determinar cuales de ellos pueden ser
+    ' utlizados por cualquer persona (public) o si por el contrario debe tener un tipo de acceso.
+
+    Class Auto
+        'Atributos (caracteristicas)
+        Public color = ""
+        Private modelo = 0
+        Public marca = ""
+        Private acceso = False
+
+        ' Metodos (funcionalidades)
+        Public Sub meterLlave(llave As String)
+            If llave = "1234" Then
+                acceso = True
+                Console.WriteLine("Llave correcta")
+            Else
+                acceso = False
+                Console.WriteLine("Llave incorrecta")
+            End If
+        End Sub
+
+        Public Sub mando(accion As String)
+            If acceso = True Then
+                If accion = "Acelerar" Then
+                    acelerarAuto()
+                End If
+            Else
+                Console.WriteLine("No se puede hacer la accion: " & accion)
+            End If
+        End Sub
+
+        Public Sub encenderAuto()
+            Console.WriteLine("Auto encendido sin problema.")
+        End Sub
+
+        Private Sub acelerarAuto()
+            Console.WriteLine("El auto acelera sin problema")
+        End Sub
+
+        Public Sub frenarAuto()
+            Console.WriteLine("El auto frena sin problema")
+        End Sub
+    End Class
+
+    Sub instanciarAuto()
+        Dim carroInstancia1 = New Auto() ' Instancia
+
+        carroInstancia1.color = "Verde"
+        'carroInstancia1.modelo = 2016
+        carroInstancia1.marca = "Toyota
+"
+        'carroInstancia1.acelerarAuto()
+        carroInstancia1.meterLlave("1234")
+        carroInstancia1.mando("Acelerar")
+        carroInstancia1.frenarAuto()
+
+        'Console.WriteLine("El carro instancia 1, tiene un color " & carroInstancia1.color & " y es modelo " & carroInstancia1.modelo)
+
+
+        Dim carroInstancia2 = New Auto() ' Instancia
+        carroInstancia2.color = "Gris"
+        'carroInstancia2.modelo = 2023
+
+        'Console.WriteLine("El carro instancia 2, tiene un color " & carroInstancia2.color & " y es modelo " & carroInstancia2.modelo)
     End Sub
 
 End Module
