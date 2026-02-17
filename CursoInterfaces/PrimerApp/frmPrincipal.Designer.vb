080f9c7a02924cb85cbc29cd4c9f40c612c457d5
Emmanuel Jaramillo <ejaramillor@tcc.com.co>
2026-02-16 23:36:08 -0500

    Inicio del proyecto - Conceptos de interfaz basicos

---
+++ CursoInterfaces/PrimerApp/frmPrincipal.Designer.vb
@@ -0,0 +1,51 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class frmPrincipal
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.btnSaludo = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'btnSaludo
+        '
+        Me.btnSaludo.Location = New System.Drawing.Point(230, 374)
+        Me.btnSaludo.Name = "btnSaludo"
+        Me.btnSaludo.Size = New System.Drawing.Size(277, 37)
+        Me.btnSaludo.TabIndex = 0
+        Me.btnSaludo.Text = "Saludo"
+        Me.btnSaludo.UseVisualStyleBackColor = True
+        '
+        'frmPrincipal
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.DeepSkyBlue
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.btnSaludo)
+        Me.Name = "frmPrincipal"
+        Me.Text = "Formulario Principal"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents btnSaludo As Button
+End Class
---
+++ CursoInterfaces/PrimerApp/frmPrincipal.vb
@@ -0,0 +1,9 @@
+﻿Public Class frmPrincipal
+    Private Sub btnSaludo_Click(sender As Object, e As EventArgs) Handles btnSaludo.Click
+        MsgBox("Hola Mundo")
+    End Sub
+
+    Private Sub btnSaludo_MouseHover(sender As Object, e As EventArgs) Handles btnSaludo.MouseHover
+        MsgBox("Que pasa chico? Vas a dar click o no?")
+    End Sub
+End Class
